91f6d66a68bc57601967c455c4b8ea27d4a005e2
Mackenzie Craig <maccraig98@gmail.com>
2018-02-15 14:11:08 -0800

    Step 3 done, added columns

---
+++ application/controllers/Views.php
@@ -0,0 +1,59 @@
+<?php
+
+class Views extends Application
+{
+    public function makePrioritizedPanel($tasks)
+    {
+        // extract the undone tasks
+        foreach ($tasks as $task) {
+            if ($task->status != 2) {
+                $undone[] = $task;
+            }
+        }
+
+        // order them by priority
+        usort($undone, "orderByPriority");
+
+        foreach ($undone as $task) {
+            $task->priority = $this->app->priority($task->priority);
+        }
+
+        foreach ($undone as $task) {
+            $converted[] = (array) $task;
+        }
+
+        // and then pass them on
+        $parms = ['display_tasks' => $converted];
+        return $this->parser->parse('by_priority', $parms, true);
+    }
+
+    public function makeCategorizedPanel($tasks)
+    {
+        $parms = ['display_tasks' => $this->tasks->getCategorizedTasks()];
+        return $this->parser->parse('by_category', $parms, true);
+    }
+
+    public function index()
+    {
+        $this->data['pagetitle'] = 'Ordered TODO List';
+        $tasks = $this->tasks->all();   // get all the tasks
+        $this->data['content'] = 'Ok'; // so we don't need pagebody
+
+        $this->data['leftside'] = $this->makePrioritizedPanel($tasks);
+        $this->data['rightside'] = $this->makeCategorizedPanel($tasks);
+
+        $this->render('template_secondary');
+    }
+}
+
+// return -1, 0, or 1 of $a's priority is higher, equal to, or lower than $b's
+function orderByPriority($a, $b)
+{
+    if ($a->priority > $b->priority) {
+        return -1;
+    } elseif ($a->priority < $b->priority) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,56 +1,58 @@
 <?php
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 class Welcome extends Application
 {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/
-	 * 	- or -
-	 * 		http://example.com/welcome/index
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
-		
-	$tasks = $this->tasks->all();   // get all the tasks
-	// count how many are not done
-	$count = 0;
-	foreach($tasks as $task) {
-        if ($task->status != 2) 
-			$count++;
-	}
-	$this->data['remaining_tasks'] = $count;
+    /**
+     * Index Page for this controller.
+     *
+     * Maps to the following URL
+     * 		http://example.com/
+     * 	- or -
+     * 		http://example.com/welcome/index
+     *
+     * So any other public methods not prefixed with an underscore will
+     * map to /welcome/<method_name>
+     * @see https://codeigniter.com/user_guide/general/urls.html
+     */
+    public function index()
+    {
+        $tasks = $this->tasks->all();   // get all the tasks
+    // count how many are not done
+    $count = 0;
+        foreach ($tasks as $task) {
+            if ($task->status != 2) {
+                $count++;
+            }
+        }
+        $this->data['remaining_tasks'] = $count;
 
-	// process the array in reverse, until we have five
-	$count = 0;
-	foreach(array_reverse($tasks) as $task) {
-		$task->priority = $this->app->priority($task->priority);
-		$display_tasks[] = (array) $task;
-		$count++;
-		if ($count >= 5) break;
-	}
-	$this->data['display_tasks'] = $display_tasks;
-	
-	// and save that as a view parameter
-	$this->data['pagebody'] = 'homepage';
-	$this->render(); 
-	}
+    // process the array in reverse, until we have five
+    $count = 0;
+        foreach (array_reverse($tasks) as $task) {
+            $task->priority = $this->app->priority($task->priority);
+            $display_tasks[] = (array) $task;
+            $count++;
+            if ($count >= 5) {
+                break;
+            }
+        }
+        $this->data['display_tasks'] = $display_tasks;
 
-	function render($template = 'template')
-{
-    $this->data['menubar'] = $this->parser->parse('_menubar', $this->config->item('menu_choices'),true);
-    // use layout content if provided
-    if (!isset($this->data['content']))
-        $this->data['content'] = $this->parser->parse($this->data['pagebody'], $this->data, true);
-    $this->parser->parse($template, $this->data);
-}
+    // and save that as a view parameter
+    $this->data['pagebody'] = 'homepage';
+        $this->render();
+    }
 
+    public function render($template = 'template')
+    {
+        $this->data['menubar'] = $this->parser->parse('_menubar', $this->config->item('menu_choices'), true);
+            // use layout content if provided
+            if (!isset($this->data['content'])) {
+                $this->data['content'] = $this->parser->parse($this->data['pagebody'], $this->data, true);
+            }
+        $this->parser->parse($template, $this->data);
+    }
 }
