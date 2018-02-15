6d80b010d593d93f0bc30d38af89a8b89985b758
Vafa <v.dehghansaei@gmail.com>
2018-02-15 12:57:35 -0800

    added the homepage features: tweaked base controller, and added tasks to the homepage-S

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -19,8 +19,38 @@ class Welcome extends Application
 	 */
 	public function index()
 	{
-		$this->data['pagebody'] = 'homepage';
-		$this->render(); 
+		
+	$tasks = $this->tasks->all();   // get all the tasks
+	// count how many are not done
+	$count = 0;
+	foreach($tasks as $task) {
+        if ($task->status != 2) 
+			$count++;
 	}
+	$this->data['remaining_tasks'] = $count;
+
+	// process the array in reverse, until we have five
+	$count = 0;
+	foreach(array_reverse($tasks) as $task) {
+		$task->priority = $this->app->priority($task->priority);
+		$display_tasks[] = (array) $task;
+		$count++;
+		if ($count >= 5) break;
+	}
+	$this->data['display_tasks'] = $display_tasks;
+	
+	// and save that as a view parameter
+	$this->data['pagebody'] = 'homepage';
+	$this->render(); 
+	}
+
+	function render($template = 'template')
+{
+    $this->data['menubar'] = $this->parser->parse('_menubar', $this->config->item('menu_choices'),true);
+    // use layout content if provided
+    if (!isset($this->data['content']))
+        $this->data['content'] = $this->parser->parse($this->data['pagebody'], $this->data, true);
+    $this->parser->parse($template, $this->data);
+}
 
 }
