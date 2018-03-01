2d48ca8b0e9ce79c97514699f2c36355e2dce2bb
Mackenzie Craig <maccraig98@gmail.com>
2018-03-01 14:50:34 -0800

    Feature job 7

---
+++ application/controllers/Roles.php
@@ -0,0 +1,10 @@
+class Roles extends Application
+{
+
+        public function actor($role = ROLE_GUEST)
+        {
+            $this->session->set_userdata('userrole',$role);
+            redirect($_SERVER['HTTP_REFERER']); // back where we came from
+        }
+
+}
