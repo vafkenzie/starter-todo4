775021583820dc1f22342457770c326a02e2f62d
Vafa <v.dehghansaei@gmail.com>
2018-03-08 12:47:48 -0800

    taskListTest

--- tests/TaskTest.php
+++ tests/TaskTest.php
@@ -16,18 +16,18 @@ class TaskTest extends TestCase
         $this->task->setTask("Finish Lab 7");
         $this->assertEquals("Finish Lab 7", $this->task->getTask());
     }
-    public function testSetPriority(){
-        $this->task->setPriority(2);
-        $this->assertEquals(2, $this->task->getPriority());
-        $this->expectException(Exception::class);
-        $this->task->setPriority("a string which is false");
-        
-    }
     public function testSetSize() {
         $this->task->setSize(3);
         $this->assertEquals(3, $this->task->getSize());
         $this->expectException(Exception::class);
         $this->task->setSize(500);
+    }
+	    public function testSetPriority(){
+        $this->task->setPriority(2);
+        $this->assertEquals(2, $this->task->getPriority());
+        $this->expectException(Exception::class);
+        $this->task->setPriority("a string which is false");
+        
     }
     public function testSetGroup() {
         $this->task->setGroup(2);
