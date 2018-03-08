4cfa554ee2930ee0753439b3741f1270336ef071
Mackenzie Craig <maccraig98@gmail.com>
2018-03-08 14:25:56 -0800

    Change parsedown case in autoload

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('parser', 'parsedown', 'session');
+$autoload['libraries'] = array('parser', 'Parsedown', 'session');
 
 /*
 | -------------------------------------------------------------------
